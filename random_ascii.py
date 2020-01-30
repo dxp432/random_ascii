ce50d564f3380b56d14520e54675c86d0061d1a5
dxp432 <dxp432@163.com>
2020-01-30 12:42:46 +0800

    first commit

---
+++ random_ascii.py
@@ -0,0 +1,6 @@
+import random
+
+# print(random.sample(
+#     """!"#$%&'()*+,-./0123456789:;< => ?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~""", 94))
+print(''.join(random.sample(
+    """!"#$%&'()*+,-./0123456789:;< => ?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~""", 96)))
